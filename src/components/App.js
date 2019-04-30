745f1aaec50d9baa789b59bd709ab117ecc5040f
Thiago Figueiredo <thiagojfreak@gmail.com>
2019-04-30 11:46:06 -0400

    Layout Complete

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Layout from "./Layout/Layout"
+
+function App() {
+  return (
+    <div className="App">
+      <Layout>
+        <p>Teste</p>
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
